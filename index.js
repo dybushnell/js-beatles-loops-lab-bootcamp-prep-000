b6130a2287927ffc2a928dade6e501a5773c0df3
Dave Bushnell <dybushnell@gmail.com>
2019-05-30 00:58:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+function theBeatlesPlay(mus, inst){
+  
+  var firstArray = [];
+  var i;
+  
+  for(i = 0; i < mus.length; i++){
+   var firstArrayContents = (mus[i] + " plays " + inst[i])
+   firstArray = [...firstArray, firstArrayContents]
+   return firstArray
+  }
+}
+
+function johnLennonFacts(jLF){
+  while (jLF.length > 0; jLF.length--)
+}
+
+function iLoveTheBeatles(num){
+  var beatlesArray = [];
+  
+  do {
+    beatlesArray[num] = "I love the Beatles!"
+    num++
+  }
+  while (num < 15);
+  return beatlesArray
+}
